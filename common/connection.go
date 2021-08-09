862be7186fdabbe3a42114e435625afc7499725e
Ramiro Cuenca <ramicuenca00@gmail.com>
2021-08-09 20:33:20 -0300

    Created GetConnection function and added to go.mod gorm.io/drivers/mysql package

---
+++ common/connection.go
@@ -0,0 +1,21 @@
+package common
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func GetConnection() *gorm.DB {
+
+	dsn := "ramiro:admin@/moneyManager?charset=utf8"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Println("Error getting the connection: ", err)
+	}
+
+	return db
+}
