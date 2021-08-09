6b13e7ca23c03c84b4551cb92142eb955963d0ed
Ramiro Cuenca <ramicuenca00@gmail.com>
2021-08-09 16:36:14 -0300

    Modified User model and added some columns

--- models/user.go
+++ models/user.go
@@ -1,6 +1,9 @@
 package models
 
 type User struct {
-	ID       int64  `json:"id"`
-	Username string `json:"username"`
+	ID          int64  `json:"id"`
+	Username    string `json:"username"`
+	Age         int64  `json:"age"`
+	Gender      bool   `json:"gender"` // 1 = Male - 0 = Female
+	Nationality string `json:"nationality"`
 }
