4ee9f8e19b5cf76256b70f281484614ab8d5ff10
Ramiro Cuenca <ramicuenca00@gmail.com>
2021-08-10 14:01:03 -0300

    Added a Validate function to each model

--- transaction/models/transaction.go
+++ transaction/models/transaction.go
@@ -1,24 +1,48 @@
 package models
 
+import (
+	"errors"
+	"time"
+)
+
 // Transaction model struc for Transaction
 type Transaction struct {
-	ID          string  `json:"id"`
-	UserID      int64   `json:"user_id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Category    string  `json:"category"`
-	Amount      float64 `json:"amount"`
-	State       bool    `json:"state"`     // 1 = done - 0 = pending
-	Recurrent   bool    `json:"recurrent"` // 1 = yes - 0 = no
+	ID          string    `json:"id"`
+	UserID      int64     `json:"user_id"`
+	Date        time.Time `json:"date"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Category    string    `json:"category"`
+	Amount      float64   `json:"amount"`
+	State       bool      `json:"state"`     // 1 = done - 0 = pending
+	Recurrent   bool      `json:"recurrent"` // 1 = yes - 0 = no
 }
 
 // Create Transaction Command
 type CreateTransactionCMD struct {
-	UserID      int64   `json:"user_id"`
-	Title       string  `json:"title"`
-	Description string  `json:"description"`
-	Category    string  `json:"category"`
-	Amount      float64 `json:"amount"`
-	State       bool    `json:"state"`     // 1 = done - 0 = pending
-	Recurrent   bool    `json:"recurrent"` // 1 = yes - 0 = no
+	UserID      int64     `json:"user_id"`
+	Date        time.Time `json:"date"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Category    string    `json:"category"`
+	Amount      float64   `json:"amount"`
+	State       bool      `json:"state"`     // 1 = done - 0 = pending
+	Recurrent   bool      `json:"recurrent"` // 1 = yes - 0 = no
+}
+
+// Validate CreateTransactionCMD
+func (c *CreateTransactionCMD) Validate() error {
+	// Check that has a positive amount
+	if c.Amount < 0.0 {
+		return errors.New("Amount must be higher than zero")
+	}
+
+	// Check it has a title a description
+	if c.Title == "" || c.Description == "" {
+		return errors.New("Title and/or description must be completed")
+	}
+
+	// Should check if Category exists
+
+	return nil
 }
--- user/models/user.go
+++ user/models/user.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 // User model structure for User
 type User struct {
 	ID          int64  `json:"id"`
@@ -15,4 +17,17 @@ type CreateUserCMD struct {
 	Age         int64  `json:"age"`
 	Gender      bool   `json:"gender"` // 1 = Male - 0 = Female
 	Nationality string `json:"nationality"`
+}
+
+func (c *CreateUserCMD) Validate() error {
+	// Validate it has a username
+	if c.Username == "" {
+		return errors.New("Username can not be empty")
+	}
+
+	if c.Age < 0 {
+		return errors.New("Age must be positive")
+	}
+
+	return nil
 }
