f712df555baea7e366e9f25f0e64bc6d4bb04b2a
Ramiro Cuenca <ramicuenca00@gmail.com>
2021-08-10 14:02:39 -0300

    Removed all the packages from go.mod in order to migrate to some lighter ones

--- go.mod
+++ go.mod
@@ -3,7 +3,4 @@ module github.com/RamiroCuenca/go-moneyManager
 go 1.16
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	gorm.io/driver/mysql v1.1.1 // indirect
-	gorm.io/gorm v1.21.12 // indirect
 )
--- user/controllers/user.go
+++ user/controllers/user.go
@@ -1,22 +1,22 @@
 package controllers
 
-import (
-	"encoding/json"
-	"net/http"
+// import (
+// 	"encoding/json"
+// 	"net/http"
 
-	"github.com/RamiroCuenca/go-moneyManager/common"
-	"github.com/RamiroCuenca/go-moneyManager/user/models"
-)
+// 	"github.com/RamiroCuenca/go-moneyManager/common"
+// 	"github.com/RamiroCuenca/go-moneyManager/user/models"
+// )
 
-func GetAllUsers(w http.ResponseWriter, r *http.Request) {
-	var users []models.User
+// func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+// 	var users []models.User
 
-	db := common.GetConnection()
-	// defer db.Close()
+// 	db := common.GetConnection()
+// 	// defer db.Close()
 
-	db.Find(&users)
+// 	db.Find(&users)
 
-	json, _ := json.Marshal(users)
+// 	json, _ := json.Marshal(users)
 
-	common.SendResponse(w, http.StatusOK, json)
-}
+// 	common.SendResponse(w, http.StatusOK, json)
+// }
